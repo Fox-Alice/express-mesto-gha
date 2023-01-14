473037ff18d2e3bc88f5c99ed89239d1346482ea
Fox Alice <elviratr@yandex.ru>
2023-01-14 22:43:04 +0300

    fix: mistakes of validation

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,19 +5,22 @@ const { JWT_SECRET_KEY, NODE_ENV } = process.env;
 
 const auth = (req, res, next) => {
   const token = req.headers.authorization;
-
+// console.log(req.headers.authorization);
   if (!token) {
     next(new UnauthorizedError('Необходима авторизация'));
   }
+
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET_KEY : 'dev_secret');
+    console.log(payload);
   } catch (err) {
-    if (err instanceof jwt.JsonWebTokenError) {
+    if (err.name === 'JsonWebTokenError') {
+      console.log(err.message);
       next(new ForbiddenError('Нет доступа'));
     } else {
-      next(err);
-    }
+    next(err);
+  }
   }
   req.user = payload;
   next();
